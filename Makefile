jwilk/fbcat

commit cf2fc238cadfe47f2f0710c91295d848267b76e9

Enable LFS build flags. They are not strictly necessary, because we only only open small files; but they don't hurt either, and they'd make Lintian happier.

Makefile
@@ -1,5 +1,6 @@
 CFLAGS ?= -g -O2
 CFLAGS += -Wall -Wextra
+CFLAGS += $(shell getconf LFS_CFLAGS)
 
 c_files = $(wildcard *.c)
 o_files = $(c_files:.c=.o)

commit 1b25d2f29c03fe5ede25ffeab430676573444426

Don't use getconf(1) to acquire LFS build flags.
The use of getconf(1) doesn't play well with cross-compiling.

Set _FILE_OFFSET_BITS=64 directly, which should be good enough for all
practical purposes.

Reverts: d254d82f9381192bdc7749fb4fb497e17baf55f9
Reverts: 659b24444fabf1119f8c813ec4758fbee62f51df
Fixes: cf2fc238cadfe47f2f0710c91295d848267b76e9

Makefile
@@ -1,8 +1,6 @@
 CFLAGS ?= -g -O2
 CFLAGS += -Wall -Wextra
-CFLAGS += $(shell getconf LFS_CFLAGS)
-LDFLAGS += $(shell getconf LFS_LDFLAGS)
-LDLIBS += $(shell getconf LFS_LIBS)
+CFLAGS += -D_FILE_OFFSET_BITS=64
 
 c_files = $(wildcard *.c)
 o_files = $(c_files:.c=.o)